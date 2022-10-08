fcaf51543ef45e2a09a5ce2945a583872bab78c9
mohity777 <56041802+mohity777@users.noreply.github.com>
2022-10-09 04:49:46 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const config = {
     preset: 'react-native',
+     transform: {
+    '^.+\\.ts?$': 'babel-jest',
+  },
     transformIgnorePatterns: ['node_modules/(?!react-native)/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     collectCoverage: true,
