7fa0a6d2f5a2343ba5e7c3a5136e481f39ff8aeb
mohity777 <56041802+mohity777@users.noreply.github.com>
2022-10-09 05:16:24 +0530

    update x

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,9 @@ const config = {
     testMatch: [
         '<rootDir>/**/*.test.(js|jsx|ts|tsx)', '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
     ],
-    transformIgnorePatterns: ['<rootDir>/node_modules/']
+    transformIgnorePatterns: [
+        'node_modules/(?!@react-native|react-native)'
+    ],
 };
 
 module.exports = config;
