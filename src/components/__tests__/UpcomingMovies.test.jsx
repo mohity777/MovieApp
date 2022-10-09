53eb0dacea1bb431f0848c056034b73f5cca64ac
mohity777 <56041802+mohity777@users.noreply.github.com>
2022-10-09 05:41:35 +0530

    Update UpcomingMovies.test.jsx

--- src/components/__tests__/UpcomingMovies.test.jsx
+++ src/components/__tests__/UpcomingMovies.test.jsx
@@ -2,25 +2,33 @@ import React from 'react';
 import 'react-native';
 import renderer from 'react-test-renderer';
 import UpcomingMovies from "../UpcomingMovies";
-import * as redux from 'react-redux'
+import * as Redux from "react-redux";
+
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useSelector: jest.fn(),
+}));
 
 const user = {
     id: 1,
     name: 'User',
   }
 
+
+describe('UpcomingMovies test cases', () => {
   const state = { 
   movies: {
   upComingMovies: [],
   upcomingLoading: false
   }
   }
-
-  jest
-    .spyOn(redux, 'useSelector')
-    .mockImplementation((callback) => callback(state))
     
-describe('UpcomingMovies test cases', () => {
+
+ beforeEach(() => {
+        Redux.useSelector.mockImplementation((callback) => {
+            return callback(state);
+        });
+    })
 
     it('renders correctly', () => {
         let component = renderer.create(<UpcomingMovies />)
