0d201435c0fc71a2b654bc1ba9dfdaa1fcf2163e
mohity_777 <marora629@gmail.com>
2022-10-09 22:49:23 +0530

    added coverageReporters

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 const config = {
-    preset: 'react-native',
-    moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "json"],
-    transform: {
-        '^.+\\.(js|jsx)?$': 'babel-jest'
-    },
-    testEnvironment: 'node',
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/$1'
-    },
-    testMatch: [
-        '<rootDir>/**/*.test.(js|jsx|ts|tsx)', '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
-    ],
-    transformIgnorePatterns: [
-        'node_modules/(?!@react-native|react-native)'
-    ],
+  preset: 'react-native',
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: [
+    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+  ],
+  transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
+  coverageReporters: ['json-summary', 'lcov'],
 };
 
 module.exports = config;
