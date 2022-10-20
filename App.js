8dcbac23d32388cd1e3af6d080703b04a41be7c1
mohit arora <marora629@gmail.com>
2022-10-21 03:22:07 +0530

    k

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppNavigationContainer from './src/navigation';
 import store from './src/store';
 import 'recyclerlistview';
-import 'zustland';
+import {StoreApi} from 'zustand'
 
 const App = () => {
 
