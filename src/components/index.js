b26c6dc31b34fa19d44864370ea3e78801cd9391
mohit arora <marora629@gmail.com>
2022-10-21 02:53:37 +0530

    l

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as  TrendingMovies} from './TrendingMovies';
 export { default as  UpcomingMovies} from './UpcomingMovies';
 export { default as  MovieCarousel} from './MovieCarousel';
-import data from './data.json';
 import 'recyclerlistview';
